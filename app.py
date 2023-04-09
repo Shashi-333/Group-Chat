0e0f9b6bd32513b4052a866bb8d846fb76662bc7
Sasikala <sasikala14474@gmail.com>
2023-04-09 19:55:41 +0530

    changes for Login page

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask import render_template,request
+
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def initial():
-    return "applied"
+    return render_template("index.html")
+
+@app.route("/login", methods = ['POST'])
+def login():
+    if request.method == "POST":
+        name = request.form.get('username')
+        form = {
+            "name": name
+        }
+        return render_template("chat.html",context = form)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host= "0.0.0.0", port = "5000" )
