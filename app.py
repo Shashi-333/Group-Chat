437f7e43660a690f6c4e3853f42b3d8a1ab8b740
Sasikala <sasikala14474@gmail.com>
2023-04-10 14:54:40 +0530

    Added form validation and model for db

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
+
 from flask import Flask
 from flask import render_template,request
-
-
+from loginForm import *
+from flask_sqlalchemy import SQLAlchemy,session
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET','POST'])
-def initial():
-    return render_template("index.html")
+#Config
+app.secret_key = "sjjfdjg&jnsf#sndf"
 
-@app.route("/login", methods = ['POST'])
+#Db congfig
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sasijana@333333@127.0.0.1:3306/groupchat'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db = SQLAlchemy(app)
+
+class admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique =True, nullable = False)
+    password = db.Column(db.String(25), unique =True, nullable = False)
+    email = db.Column(db.String(25), unique =True, nullable = False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique =True, nullable = False)
+    password = db.Column(db.String(25), unique =True, nullable = False)
+    email = db.Column(db.String(25), unique =True, nullable = False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+@app.route("/", methods = ['GET','POST'])
 def login():
-    if request.method == "POST":
-        name = request.form.get('username')
-        form = {
-            "name": name
-        }
-        return render_template("chat.html",context = form)
+    log = Loginvalidator()
+    if log.validate_on_submit():
+        return "Created"
+    return render_template("index.html",form = log)
+
+@app.route("/adduser", methods = ['POST'])
+def addusers():
+    return render_template("chat.html")
 
 
 if __name__ == "__main__":
---
+++ loginForm.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import InputRequired, Length
+
+class Loginvalidator(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=10)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=10)])
+    email = EmailField('Password', validators=[InputRequired(), Length(min=4, max=10)])
+    submit = SubmitField('Login')
