d09c6ffb5456121b81ab7d5cf500e40bdfec42dc
Sasikala <sasikala14474@gmail.com>
2023-04-11 19:55:10 +0530

    socket established

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ from flask import Flask
 from flask import render_template,request
 from loginForm import *
 from flask_sqlalchemy import SQLAlchemy,session
+from passlib.hash import pbkdf2_sha256
+from flask_socketio import SocketIO, send
+
+
 app = Flask(__name__)
 
 #Config
 app.secret_key = "sjjfdjg&jnsf#sndf"
 
 #Db congfig
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sasijana@333333@127.0.0.1:3306/groupchat'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin1234567@localhost:3306/groupchat'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
+
+#initialize Flask_socket.io
+socket = SocketIO(app)
+
+users_list=['Sasikala','Jana']
 
 class admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,7 +32,7 @@ class admin(db.Model):
         self.username = username
         self.email = email
 
-class users(db.Model):
+class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique =True, nullable = False)
     password = db.Column(db.String(25), unique =True, nullable = False)
@@ -37,13 +46,59 @@ class users(db.Model):
 def login():
     log = Loginvalidator()
     if log.validate_on_submit():
-        return "Created"
-    return render_template("index.html",form = log)
+        ad = admin.query.all()
+        us = user.query.all()
+        f = request.form
+        l=[]
+        for a in ad:
+            l.append(a.username)
+        for u in us:
+            continue
+        if f['username'] == "Sasikala":
+            if f['username'] == a.username and f['password'] == a.password :
+                return render_template("admin.html")
+            else:
+                return "Invalid Password"
+        
+        elif request.form['username'] not in l:
+            if f['username'] == u.username and f['password'] == u.password :
+                return render_template("chatroom.html")
+            else:
+                return "Invalid Password"
+            #uobj = user.query.filter_by(username == f['username'])
+            # uobj.password =  f['password']
+            # uobj.email = f['email']
+            # db.session.commit(uobj)
+    return render_template("login.html",form = log)
 
-@app.route("/adduser", methods = ['POST'])
+@app.route("/addusers", methods = ['POST'])
 def addusers():
-    return render_template("chat.html")
+    if request.method == "POST":
+        us = request.form['addusers']
+        print(users_list)
+        users_list.append(us)
+        print(users_list)
+    return render_template("admin.html")
+@app.route("/delusers", methods = ['POST'])
+def delusers():
+    if request.method == "POST":
+        if request.form['delusers'] in users_list:
+            users_list.remove(request.form['delusers'])
+            # u = user.query.filter_by(username = request.form['delusers']).delete()
+            # u.commit()
+
+    return render_template("admin.html")
+
+@app.route("/chatroom", methods = ['GET', 'POST'])
+def chatroom():
+    return render_template('chatroom.html')
+
+@socket.on('message')
+def message(data):
+    #print(data)
+
+    send(data)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host= "0.0.0.0", port = "5000" )
+    socket.run(app, debug=True)
--- loginForm.py
+++ loginForm.py
@@ -5,5 +5,5 @@ from wtforms.validators import InputRequired, Length
 class Loginvalidator(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=10)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=10)])
-    email = EmailField('Password', validators=[InputRequired(), Length(min=4, max=10)])
+    email = EmailField('Password', validators=[InputRequired(), Length(min=4, max=30)])
     submit = SubmitField('Login')
